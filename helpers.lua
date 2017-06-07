8f681269f4beda388c484a79e7a33ca3cb7602a0
johro81 <johro81@gmail.com>
2017-06-07 21:35:25 +0200

    An attempt to recreated GetServerTime()

--- helpers.lua
+++ helpers.lua
@@ -16,7 +16,7 @@ H.Debug = function(msg)
     if self.debug then
         ChatFrame1:AddMessage(msg)
     end
-end 
+end
 
 H.TableContains = function(table, val)
     for k, v in ipairs(table) do
@@ -25,4 +25,15 @@ H.TableContains = function(table, val)
         end
     end
     return false
+end
+
+H.GetServerTime = function()
+  local server_h, server_m = GetGameTime()
+  local local_h, local_m = strsplit(':', date("%H:%M"))
+
+  local server_s = server_h * 3600 + server_m * 60
+  local local_s = tonumber(local_h) * 3600 + tonumber(local_m * 60)
+  local delta_s = sever_s - local_s
+
+  return time() + delta_s
 end
